ebd100b0da5a1e550e9dfa86c7bcb71beacc8b9a
Vilson <vilson.espayos.lu@gmail.com>
2021-06-14 20:08:00 +0800

    refactored main app to pages

---
+++ src/pages/Pokedex.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react';
+import './Pokedex.module.scss';
+
+const Pokedex: React.FunctionComponent = (): ReactElement  => {
+  return (
+      <h3>Hello World</h3>
+  );
+};
+
+export default Pokedex;
